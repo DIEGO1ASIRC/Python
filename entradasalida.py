7d0fe315dc50a7c98620fa3e0bd24dbf439b0d4f
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-09-26 14:13:31 +0200

    añadido

---
+++ entradasalida.py
@@ -0,0 +1,5 @@
+numero=input("Introduce un número")
+resultado=2*int(numero)
+print(resultado)
+5
+
---
+++ medidasderectangulo.py
@@ -0,0 +1,6 @@
+base=input("Porfavor introduzca la base del rectángulo")
+altura=input("Porfavor introduzca la altura del rectángulo")
+area=float(base)*float(altura)
+perimetro=(float(base)+float(altura))*2
+print(area)
+print(perimetro)
