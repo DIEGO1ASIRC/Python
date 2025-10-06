8992c58a8c884544329c1741b0a4f0c9698f1e71
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-06 12:20:11 +0200

    Ejercicio 5

---
+++ Ejercicio 5.py
@@ -0,0 +1,4 @@
+horas = int(input("Porfavor introduzca el número de horas trabajadas: "))
+pago = int(input("Porfavor introduzca el coste por hora: "))
+resultado = pago*horas
+print("Has trabajado", horas, "a coste por hora de", pago, "da un total de ", resultado) 
