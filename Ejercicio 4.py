6c61f68ab4367b60d06644eb7dd76b8a886221f1
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-06 12:16:01 +0200

    Ejercicio 4

---
+++ Ejercicio 4.py
@@ -0,0 +1,8 @@
+suma = 3+2
+multiplicacion = 2*5 
+simplificacion_suma = 1
+simplificacion_multiplicacion= 2
+al_cuadrado_suma = suma*suma
+al_cuadrado_multiplicacion = multiplicacion*multiplicacion
+resultado = al_cuadrado_suma/al_cuadrado_multiplicacion
+print(resultado) 
