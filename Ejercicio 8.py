8f68f05ce0192d861a2f5014d026771cec8ddecd
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-06 12:36:49 +0200

    Ejercicio 8

---
+++ Ejercicio 8.py
@@ -0,0 +1,6 @@
+numero1= float(input("Porfavor introduzca el primer numero: "))
+numero2 = float(input("Porfavor introduzca el segundo numero: "))
+cociente = numero1 // numero2
+resto = numero1 % numero2
+
+print(numero1, " entre ", numero2, " da un cociente de ",cociente,"y un resto de ", resto)
