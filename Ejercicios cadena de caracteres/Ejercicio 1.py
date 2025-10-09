2cff89840188ad24ce82b5a3f81272e16749a405
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-09 10:54:29 +0200

    Ejercicio 1

---
+++ Ejercicios cadena de caracteres/Ejercicio 1.py
@@ -0,0 +1,7 @@
+nombre = input("Porfavor introduzca su nombre: ") 
+numero_veces = int(input("Porfavor introduzca un número entero: "))
+
+while numero_veces > 0:
+
+    print (nombre)
+    numero_veces= numero_veces - 1
