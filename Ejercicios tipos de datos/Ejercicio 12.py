2d0823fe0757b97e60f0c1f0fcf618198d8a5193
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-16 09:37:34 +0200

    Ejercicio 12

--- Ejercicios tipos de datos/Ejercicio 12.py
+++ Ejercicios tipos de datos/Ejercicio 12.py
@@ -4,4 +4,4 @@ descuento = 60 / 100
 precio_total_pan = cantidad_pan * 3.49
 pan_descuento = precio_total_pan * descuento
 
-print("Se han vendido", cantidad_pan, "panes que no son del día a los que se les aplicará un descuento del", descuento_numero, "dándonoso un total de", pan_descuento)
+print("Se han vendido", cantidad_pan, "panes que no son del día a los que se les aplicará un descuento del", descuento_numero, "dándonos un total de", pan_descuento)
