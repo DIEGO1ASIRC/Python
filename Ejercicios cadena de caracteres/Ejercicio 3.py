22806c5f1bcdde6e063ced33b2430bff1c192fb9
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-16 09:45:02 +0200

    Ejercicio 3

--- Ejercicios cadena de caracteres/Ejercicio 3.py
+++ Ejercicios cadena de caracteres/Ejercicio 3.py
@@ -1,3 +1,3 @@
 nombre = input("Porfavor introduzca su nombre completo: ")
 longitud_nombre = len(nombre)
-print("Tu nombre tiene ", longitud_nombre, " caracteres")
+print("Tu nombre tiene ", longitud_nombre, " letras donde", nombre.upper(), "es el nombre del usuario en mayúsculas y", longitud_nombre, "es el número de letras que tienen el nombre")
