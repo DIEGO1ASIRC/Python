8be72b6b343802fcca0864db2c6a65e046427337
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-16 10:05:49 +0200

    Ejercicio 11

--- Ejercicios tipos de datos/Ejercicio 11.py
+++ Ejercicios tipos de datos/Ejercicio 11.py
@@ -1,4 +1,4 @@
-ahorros = int(input("Porfavor introduzca la cantidad de dinero que tienes ahorrado ")) 
+ahorros = float(input("Porfavor introduzca la cantidad de dinero que tienes ahorrado ")) 
 
 interes_año = 4 / 100
 
@@ -7,4 +7,4 @@ ahorros_finales = ahorros * (1 + interes_año)
 ahorros_finales2 = ahorros_finales * 2
 ahorros_finales3 = ahorros_finales * 3
 
-print("El primer año tus ahorros serán", ahorros_finales, "tu segundo año serán de", ahorros_finales2, "tu tercer año serán de", ahorros_finales3)
+print("El primer año tus ahorros serán", round(ahorros_finales), "tu segundo año serán de", round(ahorros_finales2), "tu tercer año serán de", round(ahorros_finales3))
