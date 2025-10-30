0f361657e9c1ba344d718bb3a1db574663477a89
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:32:26 +0100

    Ejercicio 3

---
+++ Ejercicios condicionales/Ejercicio 3.py
@@ -0,0 +1,14 @@
+primer_numero = float(input("Porfavor introduzca el primer numero a dividir: "))
+segundo_numero = float(input("Porfavor introduzca el segundo numero a dividir: "))
+
+division = primer_numero % segundo_numero
+
+if division == 0 :
+
+    print ("El resultado es exacto")
+
+elif division != 0 :
+
+        print ("Error, el resultado no es exacto") 
+
+
