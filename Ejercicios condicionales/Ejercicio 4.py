d8f2c5e18e4f40a188cfe0ffd5e6a236ecb3a18c
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:33:44 +0100

    Ejercicio 4

---
+++ Ejercicios condicionales/Ejercicio 4.py
@@ -0,0 +1,14 @@
+primer_numero = float(input("Porfavor introduzca un número para comprobar si es par o impar: "))
+segundo_numero = float(2)
+
+division = primer_numero % segundo_numero
+
+if division == 0 :
+
+    print ("El numero es par")
+
+elif division != 0 :
+
+        print ("El numero es impar") 
+
+
