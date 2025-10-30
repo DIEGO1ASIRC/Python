c29d2908b8158209fe43333a95ea60137f3c0416
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:22:45 +0100

    Ejercicio 1

---
+++ Ejercicios condicionales/Ejercicio 1.py
@@ -0,0 +1,9 @@
+edad = int(input("Porfavor introduzca su edad: "))
+
+if edad >= 18:
+    print ("Eres mayor de edad")
+elif edad < 18: 
+    print ("Eres menor de edad")
+else: 
+    print ("Introduce algo correcto")
+
