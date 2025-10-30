08f8964c408005a34bba332ac3775c02e37b7424
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:26:26 +0100

    Ejercicio 2

---
+++ Ejercicios condicionales/Ejercicio 2.py
@@ -0,0 +1,13 @@
+contraseña = "contraseña"
+
+usuario_probar = input("Porfavor introduzca la contraseña correcta: ")
+
+if usuario_probar == contraseña:
+
+    print ("Has acertado máquina puedes pasar")
+
+elif usuario_probar != contraseña:
+
+    print ("Te has equivocado maquina, vuelve a intentarlo")
+
+    
