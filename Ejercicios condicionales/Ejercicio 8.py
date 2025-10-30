58a86b2ea535b1d83ce978a19b55893e51e69938
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 11:09:19 +0100

    Ejercicio 8

---
+++ Ejercicios condicionales/Ejercicio 8.py
@@ -0,0 +1,7 @@
+puntuacion = int(input("Porfavor introduzca su puntuación de rendimiento: "))
+dinero = int(2400)
+
+if puntuacion == 0.0:
+    print ("Su nivel de rendimiento es inaceptable")
+
+    elif puntuacion == 0.0:
