590f568e84ff97927c50c6581d915fd503f39be0
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:01:09 +0100

    Ejercicio 9

---
+++ Ejercicios cadena de caracteres/Ejercicio 9.py
@@ -0,0 +1,9 @@
+fecha = input("Porfavor introduzca la fecha actual: ")
+
+dia = fecha.split("/")[0]
+mes = fecha.split("/")[1]
+año = fecha.split("/")[2]
+
+
+print("Hoy es día", dia, "de", mes, "del año", año)
+
