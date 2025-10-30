48bacfd13ca6548daac8b999243992f224569de4
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 10:53:40 +0100

    Ejercicio 6

---
+++ Ejercicios condicionales/Ejercicio 6.py
@@ -0,0 +1,13 @@
+alumno = input("Porfavor introduzca su nombre: ")
+sexo = input("Porfavor introduzca su sexo: ")
+
+if alumno[0] <= "M" and sexo == "mujer":
+
+    print("Perteneces al grupo A")
+
+elif alumno[0] >= "N" and sexo == "hombre":
+    print ("Perteneces al grupo A")
+
+else:
+    print ("Perteneces al grupo B")
+
