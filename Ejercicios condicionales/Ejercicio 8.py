37e54bd5f59ee9f4cd0a09f2b27b94f0e1004bfb
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-10-30 11:49:37 +0100

    Ejercicio 8

--- Ejercicios condicionales/Ejercicio 8.py
+++ Ejercicios condicionales/Ejercicio 8.py
@@ -3,5 +3,15 @@ dinero = int(2400)
 
 if puntuacion == 0.0:
     print ("Su nivel de rendimiento es inaceptable")
+    dinero1 = (dinero*0.0)+dinero
 
-    elif puntuacion == 0.0:
+elif puntuacion == 0.4:
+
+    print ("Su nivel de rendimiento es aceptable")
+    dinero1 = (dinero*0.4)+dinero
+elif puntuacion >= 0.6
+
+    print ("Su nivel de rendimiento es meritorio")  
+    dinero1 = (dinero*0.6)+dinero
+
+    print ("El dinero total que consigues es de", dinero1)
