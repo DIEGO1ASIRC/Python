b34f142a83e65af80fb57c3d5fd3c12a07c824db
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-11-06 09:51:08 +0100

    Ejercicio 3

--- Ejercicios bucles/Ejercicio 3.py
+++ Ejercicios bucles/Ejercicio 3.py
@@ -1,15 +1,19 @@
 numero_pedido = int(input("Porfavor introduzca un numero para mostrar todos los numeros impares anteriores a el: "))
 numero = int(1)
+numero_division = int(2)
 
-division = numero_pedido % numero
 
 while numero <= numero_pedido:
 
-    numero = numero + 1
-    
+    division = numero % numero_division
+
     if division != 0 :
 
-        print (numero)
+        print(numero)
+
+    numero = numero + 1
+    
+    
 
 
 
