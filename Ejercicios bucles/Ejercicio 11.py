bc51c456a880047396a47c42c858e4b8b9d0c032
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-11-06 11:09:12 +0100

    Ejercicio 11

---
+++ Ejercicios bucles/Ejercicio 11.py
@@ -0,0 +1,6 @@
+palabra_pedida = input("Porfavor introduzca una palabra: ")
+posicion = int(0)
+while posicion < len(palabra_pedida):
+
+    print(palabra_pedida[posicion])
+    posicion = posicion + 1  
