f8fc9d3e60af06746774e6ae4f354bfefd4facac
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-11-06 10:47:42 +0100

    Ejercicio 9

---
+++ Ejercicios bucles/Ejercicio 9.py
@@ -0,0 +1,8 @@
+contraseña = "contraseña"
+
+contraseña_pedida = input("Porfavor introduzca la contraseña correcta: ")
+
+while contraseña != contraseña_pedida:
+
+    print = "Contraseña incorrecta porfavor intentelo de nuevo: "
+    contraseña_pedida = input("Porfavor introduzca la contraseña correcta: ")
