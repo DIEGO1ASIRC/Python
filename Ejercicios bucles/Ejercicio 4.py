303b6ec0493624976f462c94e845301e8db59bca
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-11-06 10:02:48 +0100

    Ejercicio 4

---
+++ Ejercicios bucles/Ejercicio 4.py
@@ -0,0 +1,8 @@
+numero_usuario = int(input("Porfavor introduzca un número: "))
+numero_cuenta_atras = numero_usuario
+
+while numero_cuenta_atras > 0:
+
+    print (numero_cuenta_atras,",", end=" ")
+
+    numero_cuenta_atras = numero_cuenta_atras - 1 
