27f5fb9e20be2a2eb7a3b412a7ead326afa15462
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-11-07 14:39:20 +0100

    intento de ejercicio 8

--- Ejercicios bucles/Ejercicio 8.py
+++ Ejercicios bucles/Ejercicio 8.py
@@ -1,9 +1,9 @@
-y = 5
-z = [1,3,5,7,9]
+y = [1,3,5,7,9]
 x = 0
-for i in range(y):
-  x = x+1
 
+for i in range(5):
+  print(y[x])
+  x = x + 1
   for j in range(x):
-    print(z[0], end=" ")
+    print("*", end=" ")
   print()
