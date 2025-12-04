908f0ce866fcc536bc8024c3fb4c77403d2a89d3
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2025-12-04 10:31:06 +0100

    Ejercicio 4

---
+++ Ejercicios listas y tuplas/Ejercicio 4.py
@@ -0,0 +1,16 @@
+
+numeros_ganadores = input("Porfavor introduzca los números ganadores de la lotería: ")
+
+lista_ganadores = []
+
+while numeros_ganadores != "salir":
+
+    lista_ganadores.append(numeros_ganadores)
+
+    numeros_ganadores = input("Porfavor introduzca los números ganadores de la lotería: ")
+
+    numeros = ",".join(lista_ganadores)
+
+if numeros_ganadores == "salir":
+
+    print ("Los números ganadores son: " + numeros )
