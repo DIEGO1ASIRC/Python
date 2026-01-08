e16d5e063725e41c1e62159632b8c76a07872f24
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-08 09:48:27 +0100

    Ejercicio 5

--- Ejercicios listas y tuplas/Ejercicio 5.py
+++ Ejercicios listas y tuplas/Ejercicio 5.py
@@ -1,10 +1,12 @@
-
 numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros_invertidos = numeros[::-1]
 
-numeros = sorted(numeros, reverse=True)
+for numero in numeros_invertidos:
 
-for numero in numeros:
+    if numero == 1:
+      
+      print(numero)  
+      
+      break
 
-    numeros_inversos = ",".join(numero)
-
-print ( numeros )
+    print(numero, end=",")
