f35eb898c734a1fb868c6f5443e28ebed98703f9
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-08 11:14:04 +0100

    diccionario

--- Ejercicios diccionarios/Ejercicio 1.py
+++ Ejercicios diccionarios/Ejercicio 1.py
@@ -1,3 +1,5 @@
 moneda =  {'Euro':'€','Dollar':'$', 'Yen':'¥'}
 moneda_pedida = input("Porfavor introduzca una moneda y yo te doy la divisa de esa moneda: ")
+
+
 print(moneda[moneda_pedida])
--- Ejercicios diccionarios/Ejercicio 2.py
+++ Ejercicios diccionarios/Ejercicio 2.py
@@ -2,4 +2,8 @@ nombre = input("Porfavor introduzca su nombre: ")
 edad = input("Porfavor introduzca su edad: ")
 direccion = input("Porfavor introduzca su direccion: ")
 telefono = input("Porfavor introduzca su telefono: ")
+persona = {"nombre":nombre,"edad":edad,"direccion":direccion,"telefono":telefono}
+
+print(nombre ,"tiene", edad, "años, vive en direccion", direccion, "y su numero de telefono es ", telefono)
+
 
