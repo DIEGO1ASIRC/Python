4a7c4ca4fe2f1abe81252e0b36aa0b917896baf6
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-09 14:39:31 +0100

    Ejercicio 4

--- Ejercicios diccionarios/Ejercicio 4.py
+++ Ejercicios diccionarios/Ejercicio 4.py
@@ -1,2 +1,4 @@
+mes = {"01":"Enero","02":"Febrero","03":"Marzo","04":"Abril","05":"Mayo","06":"Junio","07":"Julio","08":"Agosto","09":"Septiembre","10":"Octubre","11":"Noviembre","12":"Diciembre"}
 fecha_pedida = input("Porfavor introduzca una fecha con el formato dd/mm/aaaa")
 
+print(fecha_pedida.find(1))
