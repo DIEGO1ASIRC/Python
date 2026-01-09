e4739eccb484bb78213daa2e753f15ee14e02169
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-09 14:27:28 +0100

    Ejercicio 3 y empezado el 4

---
+++ Ejercicios diccionarios/Ejercicio 3.py
@@ -0,0 +1,14 @@
+fruta = {"platano":1.35,"manzana":0.80,"pera":0.85,"naranja":0.70}
+
+fruta_pedir = input("Porfavor introduzca que fruta desea comprar: ")
+
+while fruta_pedir not in fruta:
+
+    fruta_pedir = input("Porfavor introduzca que fruta desea comprar: ")
+
+kilos = int(input("Porfavor introduzca cuantos kilos quiere de la fruta: "))
+
+precio_fruta = fruta[fruta_pedir]
+precio_total = precio_fruta * kilos
+
+print(precio_total)
---
+++ Ejercicios diccionarios/Ejercicio 4.py
@@ -0,0 +1,2 @@
+fecha_pedida = input("Porfavor introduzca una fecha con el formato dd/mm/aaaa")
+
