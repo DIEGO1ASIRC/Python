38dfef1e14ebb960d0af07d3b1a6efe5cb931c88
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-16 14:38:45 +0100

    Ejercicio 9 por terminar

---
+++ Ejercicios diccionarios/Ejercicio 9.py
@@ -0,0 +1,9 @@
+opcion = "0"
+while opcion != "3":
+
+    print("1.-Añadir nueva factura")
+    print("2.-Pagar una existente")
+    print("3.-Terminar")
+    opcion = str(input("Porfavor introduzca una opcion: "))
+
+
