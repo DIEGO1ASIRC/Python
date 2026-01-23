7056bbe6abe9ea60bdab5c6a7ea9256b517f84c2
Diego Toribio <dtorper363@ieszaidinvergeles.org>
2026-01-23 14:33:41 +0100

    Ejercicio 9

--- Ejercicios diccionarios/Ejercicio 9.py
+++ Ejercicios diccionarios/Ejercicio 9.py
@@ -1,4 +1,4 @@
-facturas = []
+facturas = {}
 opcion = "0"
 while opcion != "3":
 
@@ -10,9 +10,29 @@ while opcion != "3":
 
     if opcion == "1":
 
-        numero_factura = input("Porfavor introduzca el numero de factura: ")
+        numero_factura = (input("Porfavor introduzca el numero de factura: "))
         precio_factura = int(input("Porfavor introduzca el precio de la factura: "))
 
-        facturas["numero_factura"] = precio_factura
+        facturas[numero_factura] = precio_factura
+
+    elif opcion == "2":
+
+        for factura in facturas:
+
+            print ("Numero de factura = "+ str(factura)+ " dinero a pagar = "+ str(facturas[factura]))
+
+        pagar = input("De las facturas existentes cuál desea pagar: ")
+
+        if pagar == factura:
+
+                del facturas[factura]
+
+                print("Te quedan las siguientes facturas: ")
+        
+    for factura in facturas:
+
+        print ("Numero de factura = "+ str(factura)+ " dinero a pagar = "+ str(facturas[factura]))
+    
+    
 
     
